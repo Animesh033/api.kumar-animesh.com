8a8627085183b40b2affae18e75e766a6724a72f
Animesh <kumar.animesh303@gmail.com>
2024-05-07 22:43:03 +0530

    added type module and converted from require to import

--- src/app2.js
+++ src/app2.js
@@ -41,20 +41,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // view engine setup
-// console.log("URL : ", import.meta.url)
-// const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-// const __dirname = path.dirname(__filename); // get the name of the directory
-// console.log(__dirname, __filename)
+console.log("URL : ", import.meta.url)
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+console.log(__dirname, __filename)
 
-// app.set("views", "views");
-// app.set("view engine", "ejs");
-// app.engine('ejs', ejs.__express);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.engine('ejs', ejs.__express);
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static("../public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 // app.use("/users", usersRouter);
