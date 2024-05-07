5d86c98382b20f55e2e07d0e470371890b1e5752
Animesh <kumar.animesh303@gmail.com>
2024-05-07 10:20:13 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.engine('ejs', require('ejs').__express);
 
-// app.use(logger("dev"));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
