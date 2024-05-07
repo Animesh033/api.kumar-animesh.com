8af9a62420deae9366f7950a28d409f9a55d08c6
Animesh <kumar.animesh303@gmail.com>
2024-05-07 21:17:25 +0530

    updated

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,6 +1,11 @@
-const serverless = require("serverless-http");
-const app = require('../../src/app')
+// const serverless = require("serverless-http");
+// const app = require('../../src/app')
 
-const handler = serverless(app)
-module.exports.handler = handler
+import serverless from 'serverless'
+import app from '../../src/app2'
+
+// const handler = serverless(app)
+// module.exports.handler = handler
 // module.exports.handler = app
+
+export const handler = serverless(api);
